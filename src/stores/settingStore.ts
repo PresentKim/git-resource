ee68fbf95de6eaf4b6025797449d47b2133a7863
PresentKim <debe3721@gmail.com>
2025-03-18 08:37:03 +0900

    feat(settingStore): Add filledColumnCount getter for dynamic column count
    - Introduced a new getter for filledColumnCount to dynamically calculate the number of filled columns based on the current columnCount or window width.
    - This enhances the responsiveness of the settings store and improves layout adaptability.

--- src/stores/settingStore.ts
+++ src/stores/settingStore.ts
@@ -7,23 +7,53 @@ interface SettingsStore {
   columnCount: number
   setColumnCount: (columnCount: number) => void
 
+  filledColumnCount: number
+  updateFilledColumnCount: () => void
+
   pixelated: boolean
   setPixelated: (pixelated: boolean) => void
+}
+
+const getColumnCount = () => {
+  return Number(localStorage.getItem(COLUMN_COUNT_KEY) || 0)
+}
+
+const calcColumnCount = () => {
+  const ASPECT_ITEM_SIZE = 64
+  const calculatedColumns = Math.floor(window.innerWidth / ASPECT_ITEM_SIZE)
+  return Math.max(1, calculatedColumns)
 }
 
 const GITHUB_TOKEN_KEY = 'settings.token'
 const COLUMN_COUNT_KEY = 'settings.columnCount'
 const PIXELATED_KEY = 'settings.pixelated'
-export const useSettingStore = create<SettingsStore>(() => ({
+export const useSettingStore = create<SettingsStore>((set, get) => ({
   githubToken: localStorage.getItem(GITHUB_TOKEN_KEY) || '',
-  setGithubToken: githubToken =>
-    localStorage.setItem(GITHUB_TOKEN_KEY, githubToken),
+  setGithubToken: githubToken => {
+    set({githubToken})
+    localStorage.setItem(GITHUB_TOKEN_KEY, githubToken)
+  },
 
-  columnCount: Number(localStorage.getItem(COLUMN_COUNT_KEY)) || 0,
-  setColumnCount: columnCount =>
-    localStorage.setItem(COLUMN_COUNT_KEY, columnCount.toString()),
+  columnCount: getColumnCount(),
+  setColumnCount: columnCount => {
+    set({columnCount})
+    get().updateFilledColumnCount()
+    localStorage.setItem(COLUMN_COUNT_KEY, columnCount.toString())
+  },
+  filledColumnCount: getColumnCount() || calcColumnCount(),
+  updateFilledColumnCount: () => {
+    set({filledColumnCount: get().columnCount || calcColumnCount()})
+  },
 
   pixelated: localStorage.getItem(PIXELATED_KEY) !== 'false',
-  setPixelated: (pixelated: boolean) =>
-    localStorage.setItem(PIXELATED_KEY, pixelated.toString()),
+  setPixelated: (pixelated: boolean) => {
+    set({pixelated})
+    localStorage.setItem(PIXELATED_KEY, pixelated.toString())
+  },
 }))
+
+if (typeof window !== 'undefined') {
+  window.addEventListener('resize', () => {
+    useSettingStore.getState().updateFilledColumnCount()
+  })
+}
