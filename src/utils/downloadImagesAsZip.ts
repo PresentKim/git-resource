1a912f1b700befa1e9ff1b4ea71f44e6f436f57e
PresentKim <debe3721@gmail.com>
2025-03-19 00:56:31 +0900

    feat(utils): Add function to download images as a ZIP file
    - Implemented `downloadImagesAsZip` to fetch images from a GitHub repository and package them into a ZIP file.
    - Utilizes `JSZip` for ZIP file creation and `file-saver` for downloading.

---
+++ src/utils/downloadImagesAsZip.ts
@@ -0,0 +1,26 @@
+import JSZip from 'jszip'
+import {saveAs} from 'file-saver'
+import {GithubRepo, createRawImageUrl} from './github'
+
+export const downloadImagesAsZip = async (
+  repo: GithubRepo,
+  imagePaths: string[],
+): Promise<void> => {
+  const zip = new JSZip()
+
+  // Create an array of promises for fetching images
+  const downloadPromises = imagePaths.map(async imagePath => {
+    try {
+      const response = await fetch(createRawImageUrl(repo, imagePath))
+      const blob = await response.blob()
+
+      zip.file(imagePath, blob)
+    } catch (error) {
+      console.error(`Failed to download ${imagePath}:`, error)
+    }
+  })
+
+  await Promise.all(downloadPromises)
+  const content = await zip.generateAsync({type: 'blob'})
+  saveAs(content, 'github-images.zip')
+}
